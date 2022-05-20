670d1771c0d3292af8f27ed56f1c0ca54881bd4b
krishna-nushift <krishna@nushift.in>
2022-05-20 15:57:07 +0530

    nc

--- api/main.py
+++ api/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello New World"}
 
 handler = Mangum(app=app)
--- tests/test_core.py
+++ tests/test_core.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "Hello New World"}
